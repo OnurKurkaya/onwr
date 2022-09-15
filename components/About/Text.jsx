efff09a1964489650fd23e0315a6d28af5da9579
Onur <113534595+OnurKurkaya@users.noreply.github.com>
2022-09-15 15:36:33 +0300

    Update Text.jsx

--- components/About/Text.jsx
+++ components/About/Text.jsx
@@ -1,5 +1,5 @@
 export default function Text() {
     return (
-        <p className="leading-5 text-justify tracking-wide text-lg text-gray-100 italic">Hi, I'm Onur. I have been developing myself in the front-end field for 5 years. Here you can learn a lot of information about me. You can get information about my abilities from the My Talents section. You can see the projects I have developed in the My Projects section. You can reach me from the contact section.</p>
+        <p className="leading-5 text-justify tracking-wide text-lg text-gray-100 italic">Hi, I'm Onur. I have been developing myself in the front-end field for 3 years. Here you can learn a lot of information about me. You can get information about my abilities from the My Talents section. You can see the projects I have developed in the My Projects section. You can reach me from the contact section.</p>
     );
 };
