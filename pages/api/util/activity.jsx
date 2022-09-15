642afde58ffe31bb242e8d294879ab7639cb8ac1
Onur <113534595+OnurKurkaya@users.noreply.github.com>
2022-09-15 15:14:59 +0300

    Update activity.jsx

--- pages/api/util/activity.jsx
+++ pages/api/util/activity.jsx
@@ -2,6 +2,6 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Activity(req, res) {
     res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/774591026940739585')).json()
+        await (await fetch('https://linkcord.js.org/api/v3/user/984127229212635168')).json()
     );
 };
